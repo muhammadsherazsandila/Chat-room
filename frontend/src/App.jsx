c99761a9a01238eb60ffa96483b0812fd83ef182
muhammadsherasandila <msd.sheraz046@gmail.com>
2025-06-21 18:59:47 +0500

    Updating UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ChatRoom from "./components/ChatRoom";
 import { Toaster } from "react-hot-toast";
 import Home from "./components/Home";
-import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Toaster position="top-right" />
       <BrowserRouter>
-        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/chat" element={<ChatRoom />} />
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,6 +6,7 @@ import {
   FaRegSmile,
   FaLightbulb,
   FaBolt,
+  FaComments,
 } from "react-icons/fa";
 import { motion } from "framer-motion";
 import axios from "axios";
@@ -147,6 +148,36 @@ const Home = () => {
           />
         ))}
       </div>
+
+      {/* Header */}
+      <motion.header
+        className="relative py-6 px-4 sm:px-8 flex justify-between items-center  z-10 bg-transparent backdrop-blur-md"
+        initial={{ y: -100 }}
+        animate={{ y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <motion.div
+          className="flex items-center space-x-3"
+          whileHover={{ scale: 1.05 }}
+        >
+          <motion.div
+            animate={{ rotate: [0, 10, -10, 0] }}
+            transition={{ repeat: Infinity, duration: 4 }}
+          >
+            <FaComments className="text-3xl text-blue-900" />
+          </motion.div>
+          <motion.h1
+            className="text-2xl font-bold"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.2 }}
+          >
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-gray-900 to-blue-500">
+              ROOMIFY
+            </span>
+          </motion.h1>
+        </motion.div>
+      </motion.header>
 
       {/* Main Content */}
       <main className="container mx-auto px-4 py-8 md:py-12 flex flex-col items-center relative z-10">
